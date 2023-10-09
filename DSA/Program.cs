71c56b2587f3bdae89db85a41fb33f159eddfc0e
chanducodemonk <chanducodemonk@gmail.com>
2023-10-10 00:42:46 +0530

    Stack implemented.

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using DSA.Stack;
+
+Stack stack = new Stack(3);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+
+stack.Pop();
+
+stack.Display();
+
+Console.ReadLine();
---
+++ DSA/Stack/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Stack
+{
+    public class Stack
+    {
+        private int count;
+        private int[] stack;
+
+        public Stack(int size)
+        {
+            this.stack = new int[size];
+        }
+
+        public void Push(int value)
+        {
+            if (this.count == this.stack.Length)
+            {
+                Console.WriteLine("Stack is full.");
+            }
+            else
+            {
+                this.stack[this.count++] = value;
+            }
+        }
+
+        public int Pop()
+        {
+            if (this.stack.Length == 0)
+            {
+                Console.WriteLine("Stack is Empty.");
+            }
+           
+            return this.stack[--count];
+        }
+
+        public void Display()
+        {
+            if (this.stack.Length == 0)
+            {
+                Console.WriteLine("Stack is Empty.");
+            }
+
+            for (int i = 0; i < this.stack.Length; i++)
+            {
+                Console.WriteLine(this.stack[i]);
+            }
+
+        }
+    }
+}
